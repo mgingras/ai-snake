19dde0ebb052e84c002ba800098eef2530cf0849
mgingras <martin@mgingras.ca>
2015-02-17 16:07:58 -0500

    cleaned up routes

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,9 @@
 var express = require('express'),
-          _ = require('lodash'),
-      Board = require('../lib/board.js'),
-      jade  = require('jade');
-     router = express.Router(),
-     path = require('path');
+     router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'SNAKE' });
-});
-
-/* GET new game */
-var board;
-router.get('/newGame', function(req, res, next) {
-  // console.log('before board');
-  board = new Board(); // Create a new board
-  // console.log('after board');
-  
-  var htmlBoard = jade.renderFile(path.join(__dirname, '../views') + '/board.jade', {board: board.board});
-  res.send({
-    board: board.board,
-    head: board.snake.head(),
-    food: board.food,
-    html: htmlBoard
-  });
-});
-
-router.post('/move', function(req, res, next) {
-  var body = req.body;
-  if(!board){
-    res.render('error');
-  }
-  board.snake.move(body.direction);
-  if(board.gameOver){
-    res.send('gameOver');
-    return;
-  }
-  var htmlBoard = jade.renderFile(path.join(__dirname, '../views') + '/board.jade', {board: board.board});
-  res.send({
-    board: board.board,
-    head: board.snake.head(),
-    food: board.food,
-    html: htmlBoard
-  });
-});
-
-router.post('/renderBoard', function(req,res,next) {
-  if(!req.body.board){
-    res.send({error: 'No Board'});
-  }
-  var boardToRender = req.body.board;
-  console.dir(req.body);
-  res.render('board', boardToRender);
 });
 
 module.exports = router;
